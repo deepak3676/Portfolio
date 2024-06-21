a78e9c9b0b354748dc09dc4bb242e4049bc04b51
Divyanshu <d.rawat2002@gmail.com>
2024-06-21 20:36:57 +0530

    Initial commit

---
+++ src/enivronments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    emailjs: {
+      userID: 'xizRa2arsNZZQ7wDh',
+      serviceID: 'service_uqlgu5n',
+      templateID: `template_j9q3fwb`
+    }
+  };
